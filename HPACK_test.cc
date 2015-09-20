1745fecc70e7de161a5ff444d5d0adcf3a12e5d0
ami-GS <1991.daiki@gmail.com>
2015-09-20 22:05:28 +0900

    implement test for haskell-http2-naive only

--- HPACK_test.cc
+++ HPACK_test.cc
@@ -38,38 +38,75 @@ TEST(decode_intTest, NormalTest) {
     EXPECT_EQ(16, decode_int(data[1], 4));
 }
 
+const static std::string TestCases[] = {
+    "hpack-test-case/haskell-http2-naive/",
+};
+
 TEST(encodeTest, NormalTest) {
-    std::ifstream ifs("hpack-test-case/haskell-http2-naive/story_00.json");
-    if (ifs.fail()) {
-        std::cerr  << "fail to open" << std::endl;
-    }
-    std::string str((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
-
-    picojson::value v;
-    std::string err = picojson::parse(v, str);
-    if (! err.empty()) {
-        std::cerr << err << std::endl;
-    }
-
-    picojson::object obj = v.get<picojson::object>();
-    picojson::array arr = obj["cases"].get<picojson::array>();
-    picojson::array::iterator it_seqno;
-    for (it_seqno = arr.begin(); it_seqno != arr.end(); it_seqno++) {
-        picojson::object obj_in = it_seqno->get<picojson::object>();
-        std::string wire = obj_in["wire"].to_str();
-        picojson::array json_headers = obj_in["headers"].get<picojson::array>();
-        picojson::array::iterator it_headers;
-        std::vector<header> ans_headers;
-
-        for (it_headers = json_headers.begin(); it_headers != json_headers.end(); it_headers++) {
-            picojson::object content = it_headers->get<picojson::object>();
-            picojson::object::iterator it = content.begin();
-            ans_headers.push_back(header(it->first, it->second.to_str()));
+    for (std::string testcase : TestCases) {
+        std::string call_str = "ls " + testcase + " > filename.txt";
+        int len = testcase.length();
+        char* call = new char[len+1];
+        memcpy(call, call_str.c_str(), len+1);
+        system(call);
+        delete [] call;
+        std::ifstream fnames("filename.txt");
+        if (fnames.fail()) {
+            std::cerr  << "fail to open filename.txt" << std::endl;
+        }
+        std::string field;
+        std::vector<std::string> jsons;
+        while (std::getline(fnames, field)) {
+            jsons.push_back(field);
         }
         Table* table = new Table();
-        uint8_t dst[2000];
-        int64_t len = hpack_encode(dst, ans_headers, false, false, false, table, -1);
-        //EXPECT_TRUE(0 == std::memcmp(dst, wire, len));
+        for (std::string json_file : jsons) {
+            std::string path = testcase + json_file;
+            std::ifstream ifs(path);
+
+            if (ifs.fail()) {
+                std::cerr  << "fail to open" << std::endl;
+            }
+            std::string str((std::istreambuf_iterator<char>(ifs)),
+                            std::istreambuf_iterator<char>());
+
+            picojson::value v;
+            std::string err = picojson::parse(v, str);
+            if (! err.empty()) {
+                std::cerr << err << std::endl;
+            }
+
+            picojson::object obj = v.get<picojson::object>();
+            picojson::array arr = obj["cases"].get<picojson::array>();
+            picojson::array::iterator it_seqno;
+            for (it_seqno = arr.begin(); it_seqno != arr.end(); it_seqno++) {
+                picojson::object obj_in = it_seqno->get<picojson::object>();
+                std::string wire = obj_in["wire"].to_str();
+                picojson::array json_headers = obj_in["headers"].get<picojson::array>();
+                picojson::array::iterator it_headers;
+                std::vector<header> ans_headers;
+
+                for (it_headers = json_headers.begin(); it_headers != json_headers.end(); it_headers++) {
+                    picojson::object content = it_headers->get<picojson::object>();
+                    picojson::object::iterator it = content.begin();
+                    ans_headers.push_back(header(it->first, it->second.to_str()));
+                }
+                uint8_t dst[2000];
+                int64_t len = hpack_encode(dst, ans_headers, false, false, false, table, -1);
+                uint8_t *wire_byte = new uint8_t[wire.length()/2];
+                for (int i = 0; i < wire.length(); i += 2) {
+                    char tmp_c[3];
+                    wire.copy(tmp_c, 2, i);
+                    tmp_c[2] = '\0';
+                    std::string tmp_s = std::string(tmp_c);
+                    *(wire_byte+i/2) = (uint8_t)std::stoi(tmp_s, nullptr, 16);
+                }
+
+                //EXPECT_TRUE(0 == std::memcmp(dst, wire_byte, len));
+                delete [] wire_byte;
+            }
+        }
+        delete table;
     }
 }
 
