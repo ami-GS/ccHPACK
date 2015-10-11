4e35724b730f3348743d6220756440d5bd4135d0
ami-GS <1991.daiki@gmail.com>
2015-10-11 15:38:40 +0200

    write parse_header_test

--- hpack_table_test.cc
+++ hpack_table_test.cc
@@ -39,3 +39,27 @@ TEST(get_header_test, NormalTest) {
     }
     delete table;
 }
+
+TEST(parse_header_test, NormalTest) {
+    Table *table = new Table();
+
+    int num_test = 3;
+    header expect_headers[] = {
+        header(":authority", ""),
+        header("www-authenticate", ""),
+        header("name", "val"),
+    };
+    uint8_t test_buf[] = {4, 110, 97, 109, 101, 3, 118, 97, 108};
+    int test_indices[] = {1, 61, 0};
+    bool test_indexed[] = {true, true, false};
+    int64_t expect_len[] = {0, 0, 9};
+
+    for (int i = 0; i < num_test; i++) {
+        header actual_header;
+        uint64_t len = table->parse_header(actual_header, test_indices[i], test_buf, test_indexed[i]);
+        ASSERT_EQ(expect_len[i], len);
+        ASSERT_EQ(expect_headers[i], actual_header);
+    }
+    
+    delete table;
+}
